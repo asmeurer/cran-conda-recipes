e413c61489ce65bb11700f2f9c15040b828ff97c
Trent Nelson <trent@trent.me>
2014-01-02 18:45:12 -0500

    Add fftw dependency.

--- r-fftw/build.sh
+++ r-fftw/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-$R CMD INSTALL --build .
+$R CMD INSTALL --build                                  \
+    --configure-vars="FFTW_CFLAGS=\"-I$PREFIX/include\" \
+                      FFTW_LIBS=-lfftw3" .
+
+# vim:set ts=8 sw=4 sts=4 tw=78 et:
