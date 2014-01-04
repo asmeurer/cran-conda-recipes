4b92e666359f3a85d83c4c0af5589f3309ed41c4
Trent Nelson <trent@trent.me>
2014-01-04 16:10:28 -0600

    Fix various packages.

--- r-rjags/build.sh
+++ r-rjags/build.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-$R CMD INSTALL --build .
+export JAGS_LIB=$PREFIX/lib
+export JAGS_INCLUDE=$PREFIX/include/JAGS
+export JAGS_MODULES=$PREFIX/lib/JAGS/modules-3
+
+$R CMD INSTALL  \
+    --build     \
+    --configure-args="--with-jags-lib=$JAGS_LIB
+                      --with-jags-include=$JAGS_INCLUDE
+                      --with-jags-modules=$JAGS_MODULES" .
+
+# vim:set ts=8 sw=4 sts=4 tw=78 et:
