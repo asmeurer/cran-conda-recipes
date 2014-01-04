4e5d0c9c470a4bdc60eddbd941d8d056a42897a3
Trent Nelson <trent@trent.me>
2014-01-04 05:21:39 -0500

    Fix logic with recommended packages.

--- r-boot/build.sh
+++ r-boot/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+cp DESCRIPTION DESCRIPTION.old
+cat DESCRIPTION.old | grep -v '^Priority: ' > DESCRIPTION
+
 $R CMD INSTALL --build .
--- r-lattice/bld.bat
+++ r-lattice/bld.bat
@@ -1,1 +1,5 @@
+
+copy DESCRIPTION DESCRIPTION.old
+type DESCRIPTION.old | find /v "Priority: recommended" > DESCRIPTION
+
 %R_CMD% INSTALL --build .
