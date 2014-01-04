130110d5b3581bd3fc24989bd3905f88b8389c7a
Trent Nelson <trent@trent.me>
2014-01-04 02:05:11 -0500

    Fix r-roracle/build.sh.

--- r-roracle/build.sh
+++ r-roracle/build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-$R CMD INSTALL --build .
+CONFIG_ARGS="--with-oci-lib=$PREFIX/lib --with-oci-inc=$PREFIX/include"
+CONFIG_VARS="CFLAGS=\"-I$PREFIX/include\" LDFLAGS=\"-L$PREFIX/lib\""
+
+$R CMD INSTALL                          \
+    --build                             \
+    --configure-args="$CONFIG_ARGS"     \
+    --configure-vars="$CONFIG_VARS"     \
+    .
